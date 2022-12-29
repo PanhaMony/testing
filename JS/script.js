eb1b4e9bb159e2259f163e8587e7e74b17a108e1
PanhaMony <121440692+PanhaMony@users.noreply.github.com>
2022-12-29 14:51:15 +0700

    testing Jquery

--- JS/script.js
+++ JS/script.js
@@ -8,8 +8,10 @@ $(document).ready(function(){
       greet = "Good Morning Everyone...";
     else if (hrs >= 12 && hrs <= 17)
       greet = "Good Afternoon  Everyone...";
-    else if (hrs >= 17 && hrs <= 24)
+    else if (hrs >= 17 && hrs <= 22)
       greet = "Good Evening Everyone...";
+    else if (hrs >= 22 && hrs <= 24)
+      greet = "Good Night Everyone...";
     
     // document.getElementById('headtext').innerHTML ='<b>' + greet + '</b>';
     $('#headtext').empty().append(greet +"You guys are real heroes!.");
